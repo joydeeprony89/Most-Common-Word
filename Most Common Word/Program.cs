71530172d6891e224a015f9cc9d0c55ea947d1a5
Jaydeep Shil <ED8A@eurofins.com>
2021-06-29 15:51:22 +0530

    Add project files.

---
+++ Most Common Word/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Most_Common_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+            //string paragraph = "a, a, a, a, b,b,b,c, c";
+            var banned = new string[] { "hit" };
+            Console.WriteLine(MostCommonWord(paragraph, banned));
+        }
+
+        static string MostCommonWord(string paragraph, string[] banned)
+        {
+            Dictionary<string, int> hash = new Dictionary<string, int>();
+            string remove = Regex.Replace(paragraph, @"[\W_]$", "");
+            string result = Regex.Replace(remove, @"[\W_]+", " ");
+            result = result.ToLower();
+            var arr = result.Split(' ');
+            foreach (var item in arr)
+            {
+                if (hash.ContainsKey(item))
+                {
+                    hash[item] += 1;
+                }
+                else
+                {
+                    hash[item] = 1;
+                }
+            }
+
+            return hash.Where(x=>!banned.Contains(x.Key))
+                .ToDictionary(x=>x.Key,x=>x.Value)
+                .OrderByDescending(x => x.Value)
+                .ToDictionary(x=>x.Key,x=>x.Value)
+                .FirstOrDefault().Key;
+
+        }
+    }
+}
